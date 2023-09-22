b7f9a44efa5669f7dfae6a0ee6b6c7f7d89d9e46
Kerrion Siokwu <ladykerrion@sky.com>
2023-09-22 17:11:09 +0100

    route handler for the work  URL working

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+app.post("/work", (req, res) => {
+  res.render("work.ejs");
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
