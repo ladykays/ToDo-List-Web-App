2c41f2b4e026a5771efc7e6bb3906757ca750482
Kerrion Siokwu <ladykerrion@sky.com>
2023-09-24 00:47:59 +0100

    code refactor. All functionalities working

--- index.js
+++ index.js
@@ -1,63 +1,50 @@
-import express from "express";
-//import bodyParser from "body-parser"; //No need for this anymore as Express 4.16+ now has bodyparser included in its default package
+import express from 'express';
 
 const app = express();
-const port = 3000;
-let items = [];
-let workItems = [];
+let port = 3000;
+const listItems = [];
+const workListItems = [];
 
 //link static files making sure the css works properly
 app.use(express.static("public"));
 
-//app.use(bodyParser.urlencoded({ extended: true }));//No need for this anymore as Express 4.16+ now has bodyparser included in its default package
-app.use(express.urlencoded()); //use this in place of the bodyparser code above. This code parses URL-encoded bodies
-
+app.use(express.urlencoded()); //use this to avoid "Cannot read properties of undefined ..." error. It parses URL-encoded bodies
 
 app.get("/", (req, res) => {
-  const dayName = dayOfWeek();
-  const monthName = month();
-  res.render("index.ejs", { 
-    day: dayName, 
-    month: monthName,
-    todoList: items,
+  //generate a string that represents the current date in the format "Weekday, Month Day," where "Weekday" is the full weekday name, "Month" is the full month name, and "Day" is the numeric day of the month.
+  let date = new Date().toLocaleDateString("en", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  }); 
+  res.render("index.ejs", {
+    todaysDate: date,
+    todoList: listItems,
+    isHome: true,
   });
 });
 
 app.post("/", (req, res) => {
   const item = req.body.newItem;
-  items.push(item);
+  listItems.push(item);
+  console.log("List Items: " + listItems);
   res.redirect("/");
 });
 
 app.get("/work", (req, res) => {
-  res.render("work.ejs", {
-    workTodoList: workItems,
-  });
+  res.render("work.ejs", { 
+    workList: workListItems,
+    isHome: false,
+   });
 });
 
-app.post("/work", (req, res) => { 
-  let workItem = req.body.newItem;
-  workItems.push(workItem);
+app.post("/work", (req, res) => {
+  const workItem = req.body.newItem;
+  workListItems.push(workItem);
+  console.log("Work Items: " + workListItems);
   res.redirect("/work");
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
-
-function dayOfWeek() {
-  let daysArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  const today = new Date();
-  let dayIndex = today.getDay();
-  
-  return daysArr[dayIndex];
-};
-
-function month() {
-  let monthsArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-  let monthIndex = new Date().getMonth();
-
-  return monthsArr[monthIndex];
-  
-};
+  console.log(`listening on port ${port}`);
+})
